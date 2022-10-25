bb7e90da83edd4467373d9862c1b0e71909d8366
diac <nikolai.gladkikh@gmail.com>
2022-10-25 13:55:06 +0300

    8. HTML form. Создание кандидата. [#504845]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,7 +3,13 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Controller
 public class CandidateController {
@@ -14,5 +20,26 @@ public class CandidateController {
     public String candidates(Model model) {
         model.addAttribute("candidates", candidateStore.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute(
+                "candidate",
+                new Candidate(
+                        0,
+                        "Заполните имя",
+                        "Заполните описание",
+                        Date.valueOf(LocalDateTime.now().toLocalDate())
+                )
+        );
+        return "addCandidate";
+    }
+
+    @PostMapping("/formAddCandidate")
+    public String storeCandidate(@ModelAttribute Candidate candidate) {
+        System.out.println(candidate.getName());
+        candidateStore.create(candidate);
+        return "redirect:/candidates";
     }
 }
--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -11,12 +11,14 @@ public class CandidateStore {
 
     private static final CandidateStore INST = new CandidateStore();
 
+    private volatile int currentId = 1;
+
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private CandidateStore() {
-        candidates.put(1, new Candidate(1, "John Smith", "Extremely capable but emotionally unstable", Date.valueOf("2022-01-01")));
-        candidates.put(2, new Candidate(2, "Bob Brown", "Very disciplined but lacks ambition", Date.valueOf("2022-01-01")));
-        candidates.put(3, new Candidate(3, "Bill Miller", "Mostly unreliable but has some useful connections", Date.valueOf("2022-01-01")));
+        create(new Candidate(1, "John Smith", "Extremely capable but emotionally unstable", Date.valueOf("2022-01-01")));
+        create(new Candidate(2, "Bob Brown", "Very disciplined but lacks ambition", Date.valueOf("2022-01-01")));
+        create(new Candidate(3, "Bill Miller", "Mostly unreliable but has some useful connections", Date.valueOf("2022-01-01")));
     }
 
     public static CandidateStore instOf() {
@@ -25,5 +27,10 @@ public class CandidateStore {
 
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    public synchronized Candidate create(Candidate candidate) {
+        candidate.setId(currentId);
+        return candidates.putIfAbsent(currentId++, candidate);
     }
 }

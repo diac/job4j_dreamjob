b15f076aa5cdb961375e0d15a82fb38dbdbdf3be
diac <nikolai.gladkikh@gmail.com>
2022-10-28 09:36:53 +0300

    4. Многопоточность в базе данных [#504860 #355521]: Убрать кавычки из объявления полей email и password

--- db/scripts/update_003.sql
+++ db/scripts/update_003.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  "email" varchar,
-  "password" TEXT
+  email varchar,
+  password TEXT
 );
 
 ALTER TABLE users ADD CONSTRAINT email_unique UNIQUE (email);

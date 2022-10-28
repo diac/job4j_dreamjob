ffcf498585ec5be3a30db65979038ecd9449c3e2
diac <nikolai.gladkikh@gmail.com>
2022-10-28 09:11:51 +0300

    3. Тестирование базы данных. Liquibase H2 [#504862 #355480]: Добавить заглушки для полей в тестах

--- src/test/java/ru/job4j/dreamjob/store/CandidateDBStoreTest.java
+++ src/test/java/ru/job4j/dreamjob/store/CandidateDBStoreTest.java
@@ -3,6 +3,9 @@ package ru.job4j.dreamjob.store;
 import org.junit.jupiter.api.Test;
 import ru.job4j.dreamjob.Main;
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.City;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,7 +14,14 @@ public class CandidateDBStoreTest {
     @Test
     public void whenCreateCandidate() {
         CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
-        Candidate candidate = new Candidate(1, "Java Middle");
+        Candidate candidate = new Candidate(
+                0,
+                "Java Middle",
+                "",
+                LocalDateTime.now(),
+                new City(0, ""),
+                new byte[0]
+        );
         store.add(candidate);
         Candidate candidateInDb = store.findById(candidate.getId());
         assertThat(candidateInDb.getName()).isEqualTo(candidate.getName());
@@ -20,7 +30,14 @@ public class CandidateDBStoreTest {
     @Test
     public void whenUpdateCandidate() {
         CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
-        Candidate candidate = new Candidate(1, "Java Middle");
+        Candidate candidate = new Candidate(
+                0,
+                "Java Middle",
+                "",
+                LocalDateTime.now(),
+                new City(0, ""),
+                new byte[0]
+        );
         store.add(candidate);
         candidate.setName("Java Senior");
         boolean updateSuccess = store.update(candidate);
